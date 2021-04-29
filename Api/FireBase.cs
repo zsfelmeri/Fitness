c4a85846ea98f9dd88ca1ac36d4b11b780b2ad85
zsfelmeri <felemri.zsolt@yahoo.com>
2021-04-29 17:32:29 +0300

    insert, delete, edit clients done

--- Api/FireBase.cs
+++ Api/FireBase.cs
@@ -7,6 +7,7 @@ using FireSharp.Config;
 using FireSharp.Interfaces;
 using FireSharp.Response;
 using Fitness.Data;
+using Newtonsoft.Json;
 
 namespace Fitness.Api
 {
@@ -35,16 +36,88 @@ namespace Fitness.Api
            
         }
         
-        public async Task<List<Client>> GetClients(string table)
+        public async Task<List<Client>> GetClients()
         {
-            FirebaseResponse response = await client.GetTaskAsync(table);
-            return response.ResultAs<List<Client>>();
+            FirebaseResponse response = await client.GetTaskAsync("Clients");
+            Dictionary<string, ClientNeeded> dict = new Dictionary<string, ClientNeeded>();
+            dict = JsonConvert.DeserializeObject<Dictionary<string, ClientNeeded>>(response.Body);
+
+            List<Client> cls = new List<Client>();
+            if (dict.Count != 0)
+            {
+                foreach (var c in dict)
+                {
+                    Client cc = new Client
+                    {
+                        id = c.Key,
+                        address = c.Value.address,
+                        barCode = c.Value.barCode,
+                        email = c.Value.email,
+                        comments = c.Value.comments,
+                        insertedDate = c.Value.insertedDate,
+                        isDeleted = c.Value.isDeleted,
+                        name = c.Value.name,
+                        personalIdentity = c.Value.personalIdentity,
+                        photo = c.Value.photo,
+                        telefon = c.Value.telefon
+                    };
+                    cls.Add(cc);
+                }
+            }
+
+            return cls;
         }
 
-        public async Task<Clientid> GetClientById(string id)
+        public async Task<Client> GetClientById(string id)
         {
             FirebaseResponse response = await client.GetTaskAsync($"Clients/{id}");
-            return response.ResultAs<Clientid>();
+            Client c = JsonConvert.DeserializeObject<Client>(response.Body);
+            c.id = id;
+
+            return c;
+        }
+
+        public async void UpdateClient(Client c)
+        {
+            ClientNeeded clientNeeded = new ClientNeeded
+            {
+                address = c.address,
+                barCode = c.barCode,
+                email = c.email,
+                comments = c.comments,
+                insertedDate = c.insertedDate,
+                isDeleted = c.isDeleted,
+                name = c.name,
+                personalIdentity = c.personalIdentity,
+                photo = c.photo,
+                telefon = c.telefon
+            };
+
+            await client.UpdateTaskAsync($"Clients/{c.id}", clientNeeded);
+        }
+
+        public async void InsertClient(Client c)
+        {
+            ClientNeeded cn = new ClientNeeded
+            {
+                address = c.address,
+                email = c.email,
+                barCode = c.barCode,
+                isDeleted = c.isDeleted,
+                comments = c.comments,
+                insertedDate = c.insertedDate,
+                name = c.name,
+                personalIdentity = c.personalIdentity,
+                photo = c.photo,
+                telefon = c.telefon
+            };
+
+            await client.PushTaskAsync($"Clients", cn);
+        }
+
+        public async void DeleteClient(string id)
+        {
+            await client.DeleteTaskAsync($"Clients/{id}");
         }
     }
 }
--- Data/Client.cs
+++ Data/Client.cs
@@ -8,14 +8,24 @@ namespace Fitness.Data
 
     public class Client
     {
-        public Clientid clientId { get; set; }
+        public string id { get; set; }
+        public string address { get; set; }
+        public string barCode { get; set; }
+        public string[] comments { get; set; }
+        public string email { get; set; }
+        public string insertedDate { get; set; }
+        public int isDeleted { get; set; }
+        public string name { get; set; }
+        public string personalIdentity { get; set; }
+        public string photo { get; set; }
+        public string telefon { get; set; }
     }
 
-    public class Clientid
+    public class ClientNeeded
     {
         public string address { get; set; }
         public string barCode { get; set; }
-        public List<string> comments { get; set; }
+        public string[] comments { get; set; }
         public string email { get; set; }
         public string insertedDate { get; set; }
         public int isDeleted { get; set; }
